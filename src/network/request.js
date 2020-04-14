4e6cff7f7433d02bfb178a4b6ea8754a81587ed1
Tianz-qin <907641025@qq.com>
2020-04-14 09:48:03 +0800

    ''

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
 //1.创建axios的实例
   const instance = axios.create({
-    baseURL:'http://106.54.54.237:8000/api/wh',
+    baseURL:'http://152.136.185.210:8000/api/h8',
     timeout:10000
   });
 
@@ -37,7 +37,7 @@ export function request(config) {
 
 export function request2(config) {
   const instance = axios.create({
-    baseURL:'http://123.207.32.32:8000/api/wh',
+    baseURL:'http://152.136.185.210:8000/api/h8',
     timeout:10000
   });
   return instance(config)
